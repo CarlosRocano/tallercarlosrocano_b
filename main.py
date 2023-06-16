d27bba6175bf1a4d28dd71b19c828ab2222a71fe
CarlosRocano <136109057+CarlosRocano@users.noreply.github.com>
2023-06-16 17:38:38 +0000

    Creacion modulo 1

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print("sqrt: ", math.sqrt(25))
+print("Suma: ", sumar(2, 3))
+print("Resta: ", restar(2, 3))
+print("Multiplicacion: ", multiplicar(2, 3))
+print("Division: ", dividir(2, 3))
+print("Distancia: ", distancia(1, 1, 3, 4))
